38838399b23ae4103b003fadfb654e8c226a684f
Arjuna Rawat <arjunarawatyt@gmail.com>
2024-09-20 23:42:38 +0530

    code fixed

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
     basePath: "/2048-in-react",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;
-  
+};
+
+export default nextConfig;
