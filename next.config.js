c0b6bdb2d38a3d0431efd5af746e2400f0906f46
Arjuna Rawat <arjunarawatyt@gmail.com>
2024-09-21 01:01:22 +0530

    done

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
     output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-};
+    reactStrictMode: true}
 
-export default nextConfig;
+module.exports = nextConfig
